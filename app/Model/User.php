8f70d6e1bdbcaa03686f78a9248a18b1aa6f020d
John Doe <thuatnt2@gmail.com>
2013-08-14 00:38:34 +0700

    qwe

---
+++ app/Model/User.php
@@ -0,0 +1,85 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * User Model
+ *
+ * @property Post $Post
+ */
+class User extends AppModel {
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public $validate = array(
+        'user_name' => array(
+            'minlength' => array(
+                'rule' => array('minlength', '3'),
+                'message' => 'username must be at least 3 characters',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+            'maxlength' => array(
+                'rule' => array('maxlength', '30'),
+                'message' => 'username\'s length is 30 characters',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'password' => array(
+            'minlength' => array(
+                'rule' => array('minlength', '0'),
+                'message' => 'password must be at least 6 characters',
+                //'allowEmpty' => false, // string not empty  
+                //
+             // 'required' => true, // Presence
+            //  
+            // User.create(name=> 'a', email => 'e')
+            // User.new(name=> 'a2',pass=> '123' email => 'e2')
+            //'last' => false, // Stop validation after this rule
+            // 'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'email' => array(
+            'email' => array(
+                'rule' => array('email'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
+
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
+
+    /**
+     * hasMany associations
+     *
+     * @var array
+     */
+    public $hasMany = array(
+        'Post' => array(
+            'className' => 'Post',
+            'foreignKey' => 'user_id',
+            'dependent' => true,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        )
+    );
+
+}
---
+++ app/Test/Fixture/UserFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * UserFixture
+ *
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 45, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 45, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_name' => 'Lorem ipsum dolor sit amet',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'email' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
